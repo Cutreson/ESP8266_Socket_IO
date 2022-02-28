c30e088dd0d3d1022c54a35a730151abadc7726e
Cutreson <dinhhongsonpt@gmail.com>
2022-03-01 00:41:36 +0700

    esp8266 socket io template

--- public/Script.js
+++ public/Script.js
@@ -1,7 +1,20 @@
-var socket = io("https://http://localhost:3000")
+var socket = io("https://esp8266-socket-io.herokuapp.com/");
 
-$(document).ready(function() {
-    $("#button").click(function() {
-        socket.emit("cilent-send-data");
+socket.on("server-send-browser", function(data) {
+    if (data == "led on") {
+        $("#console").text("LED ON");
+    } else {
+        $("#console").text("LED OFF");
+    }
+});
+
+$(function() {
+    $("#button").on("click", function() {
+        if ($("#console").text() == "LED ON") {
+            socket.emit("browser-send-data", "off led");
+        } else {
+            socket.emit("browser-send-data", "on led");
+        }
     })
+
 });
